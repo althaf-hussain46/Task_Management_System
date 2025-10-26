15b317bd0ef1cd5a19205754cd6a83a9a994c685
althaf-hussain46 <althafhussain2k3@gmail.com>
2025-10-26 22:02:46 +0530

    Created:logout page

--- logout.php
+++ logout.php
@@ -1,3 +1,10 @@
 <?php
 include_once("./Config/config.php");
+include_once(DIR_URL . "./Connection/dbConnection.php");
+
+unset($_SESSION['success_notification']);
+unset($_SESSION['failure_notification']);
+unset($_SESSION['project_search_result']);
+session_destroy();
+
 header("Location:" . BASE_URL . "/index.php");
