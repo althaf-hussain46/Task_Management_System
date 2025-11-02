56a2b9d139c2d335543a23a7fc480053b44d2d43
althaf-hussain46 <althafhussain2k3@gmail.com>
2025-11-02 22:02:18 +0530

    Created: New Module Created

---
+++ Admin/totalUsers.php
@@ -0,0 +1,43 @@
+<?php
+include_once("../Config/config.php");
+include_once(DIR_URL . "/Connection/dbConnection.php");
+include_once(DIR_URL . "/Includes/header.php");
+
+?>
+
+<body>
+
+
+    <div id="grid-container-offcanvas">
+        <div id="header">
+            <?php include_once(DIR_URL . "/Includes/adminNavbar.php"); ?>
+        </div>
+
+        <div id="offcanvas"><?php include_once(DIR_URL . "/Includes/sidebar.php"); ?></div>
+        <div id="main-content"><?php include_once(DIR_URL . "/Includes/totalUsersContent.php"); ?></div>
+        <div id="developer">Developed by</div>
+        <div id="name">Althaf Hussain J</div>
+        <div id="contact">
+            <ul style="list-style-type:none">
+                <li style="text-align:center">9094095610</li>
+                <li>althafhussain2k3@gmail.com</li>
+            </ul>
+        </div>
+
+
+    </div>
+</body>
+
+
+
+
+<div>
+
+
+</div>
+
+
+
+<?php
+include_once(DIR_URL . "/Includes/footer.php");
+?>
